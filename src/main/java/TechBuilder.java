60fc130919c683e9bd275a903e3c5edac6bd254d
Nathanael Swecker <you@example.com>
2021-11-17 11:17:00 -0800

    Actually adding TechBuilder.java this time.

---
+++ src/main/java/TechBuilder.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Abstract builder class for Builder pattern.
+ */
+public abstract class TechBuilder {
+    public abstract void adjNetIncome();
+    public abstract void adjRevenue();
+    public abstract void adjMarketShare();
+    public abstract void adjApproval();
+    public abstract void adjLevel();
+
+}
