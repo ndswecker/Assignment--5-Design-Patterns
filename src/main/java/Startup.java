711d8bfe450c5890ab234bdfd56ef25f1addcc3c
Nathanael Swecker <you@example.com>
2021-11-17 17:10:49 -0800

    Checkstyle changes

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -20,71 +20,77 @@ public class Startup {
         return sendable;
     }
     
-  public void adjNetIncome(double amount) {
-      this.netIncome += amount;
-  }
-  public double getNetIncome() {
-      return this.netIncome;
-  }
+    public void adjNetIncome(double amount) {
+        this.netIncome += amount;
+    }
+    
+    public double getNetIncome() {
+        return this.netIncome;
+    }
   
-  public void adjRevenue(double amount) {
-      this.revenue += amount;
-  }
-  public double getRevenue() {
-      return this.revenue;
-  }
+    public void adjRevenue(double amount) {
+        this.revenue += amount;
+    }
+    
+    public double getRevenue() {
+        return this.revenue;
+    }
   
-  public void adjMarketShare(double amount) {
-      this.marketShare += amount;
-  }
-  public double getMarketShare() {
-      return this.marketShare;
-  }
+    public void adjMarketShare(double amount) {
+        this.marketShare += amount;
+    }
+    
+    public double getMarketShare() {
+        return this.marketShare;
+    }
   
-  public void adjPublicApproval(double amount) {
-      this.publicApproval += amount;
-  }
-  public double getPublicApproval() {
-      return this.publicApproval;
-  }
+    public void adjPublicApproval(double amount) {
+        this.publicApproval += amount;
+    }
+    
+    public double getPublicApproval() {
+        return this.publicApproval;
+    }
   
-  public void adjLevel(int adj) {
-      this.level++;    
-  }
-  public int getLevel() {
-      return this.level;
-  }
+    public void adjLevel(int adj) {
+        this.level++;    
+    }
+    
+    public int getLevel() {
+        return this.level;
+    }
   
-  public void adjName(String newName) {
-      this.name = newName;
-  }
-  public String getName() {
-      return this.name;
-  }
+    public void adjName(String newName) {
+        this.name = newName;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
     
     // See https://www.linkedin.com/pulse/four-basic-types-technology-company-why-you-need-know-ben-blomerley
-//    public enum TechType {
-//        HARDWARE, SERVICE, BUSINESSEXT, MARKETPLACE
-//    }
-//    
-//    public TechType techType;
-//    
-//    public Startup(TechType type) {
-//        
-//        switch(type){
-//            case HARDWARE:
-//                this.techType = TechType.HARDWARE;
-//                break;
-//            case SERVICE:
-//                this.techType = TechType.SERVICE;
-//                break;
-//            case BUSINESSEXT:
-//                this.techType = TechType.BUSINESSEXT;
-//                break;
-//            case MARKETPLACE:
-//                this.techType = TechType.MARKETPLACE;
-//                break;
-//        }
-//    }
+    //    public enum TechType {
+    //        HARDWARE, SERVICE, BUSINESSEXT, MARKETPLACE
+    //    }
+    //    
+    //    public TechType techType;
+    //    
+    //    public Startup(TechType type) {
+    //        
+    //        switch(type){
+    //            case HARDWARE:
+    //                this.techType = TechType.HARDWARE;
+    //                break;
+    //            case SERVICE:
+    //                this.techType = TechType.SERVICE;
+    //                break;
+    //            case BUSINESSEXT:
+    //                this.techType = TechType.BUSINESSEXT;
+    //                break;
+    //            case MARKETPLACE:
+    //                this.techType = TechType.MARKETPLACE;
+    //                break;
+    //        }
+    //    }
 
 }
--- src/main/java/StartupBuilder.java
+++ src/main/java/StartupBuilder.java
@@ -4,12 +4,19 @@ package main.java;
  * Abstract Builder Class for Builder pattern.
  */
 public abstract class StartupBuilder {
+    
     public abstract void startRevenue();
+    
     public abstract void startMarketShare();
+    
     public abstract void startNetIncome();
+    
     public abstract void startApproval();
+    
     public abstract void startLevel();
+    
     public abstract Startup getStartup();
+    
     public abstract void startName(String name);
 
 }
