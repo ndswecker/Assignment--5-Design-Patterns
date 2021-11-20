06af1b25fcf0ef8993f33f866ca723411ee8570e
Nathanael Swecker <you@example.com>
2021-11-19 18:24:21 -0800

    Add TechGiantTest.java

---
+++ src/test/TechGiantTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import main.java.*;
+
+public class TechGiantTest {
+
+}
