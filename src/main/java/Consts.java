928d651139f0464789efd508cbbcfec9fc411162
Nathanael Swecker <you@example.com>
2021-11-19 19:28:08 -0800

    Add Consts.java
    Used to set or change basic TG and SU attr for alternative settings.

---
+++ src/main/java/Consts.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public final class Consts {
+    
+    public static final double TGHIGH = 750.0;
+    public static final double TGMID = 500.0;
+    public static final double TGLOW = 250.0;
+}
