bf6cc4d1abad01878522d7c5c67c0313d0092023
Nathanael Swecker <you@example.com>
2021-11-20 11:23:37 -0800

    Now we Actually have 4 kinds of Startups

---
+++ src/main/java/StartupBE.java
@@ -0,0 +1,63 @@
+package main.java;
+
+/**
+* Class: StartupBE is a Business extension tech startup. 
+*/
+
+public class StartupBE extends StartupBuilder {
+    
+    private Startup newStartup = new Startup();
+    
+    @Override
+    public void startRevenue() {
+        newStartup.adjRevenue(Consts.SU_MID);
+        
+    }
+
+    @Override
+    public void startMarketShare() {
+        newStartup.adjMarketShare(Consts.SU_MID);
+        
+    }
+
+    @Override
+    public void startNetIncome() {
+        newStartup.adjNetIncome(Consts.SU_HIGH);
+        
+    }
+
+    @Override
+    public void startApproval() {
+        newStartup.adjPublicApproval(Consts.SU_LOW);
+        
+    }
+
+    @Override
+    public void startLevel() {
+        newStartup.adjLevel(Consts.START_LEVEL);
+        
+    }
+
+    @Override
+    public Startup getStartup() {
+        return newStartup;
+        
+    }
+
+    @Override
+    public void startName(String name) {
+        newStartup.adjName(name);
+        
+    }
+    
+    @Override
+    public void startTechType(TechType type) {
+        newStartup.adjTechType(type);
+    }
+    
+    @Override
+    public void startIndependent() {
+        newStartup.makeIndependent();
+    }
+
+}
---
+++ src/main/java/StartupSE.java
@@ -0,0 +1,58 @@
+package main.java;
+
+public class StartupSE extends StartupBuilder {
+    private Startup newStartup = new Startup();
+    
+    @Override
+    public void startRevenue() {
+        newStartup.adjRevenue(Consts.SU_MID);
+        
+    }
+
+    @Override
+    public void startMarketShare() {
+        newStartup.adjMarketShare(Consts.SU_MID);
+        
+    }
+
+    @Override
+    public void startNetIncome() {
+        newStartup.adjNetIncome(Consts.SU_HIGH);
+        
+    }
+
+    @Override
+    public void startApproval() {
+        newStartup.adjPublicApproval(Consts.SU_LOW);
+        
+    }
+
+    @Override
+    public void startLevel() {
+        newStartup.adjLevel(Consts.START_LEVEL);
+        
+    }
+
+    @Override
+    public Startup getStartup() {
+        return newStartup;
+        
+    }
+
+    @Override
+    public void startName(String name) {
+        newStartup.adjName(name);
+        
+    }
+    
+    @Override
+    public void startTechType(TechType type) {
+        newStartup.adjTechType(type);
+    }
+    
+    @Override
+    public void startIndependent() {
+        newStartup.makeIndependent();
+    }
+
+}
