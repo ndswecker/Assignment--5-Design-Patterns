318f7075245d58a2d58aab692e719d013a6455c9
Nathanael Swecker <you@example.com>
2021-11-21 19:06:53 -0800

    Finish basic tests of startup attacks
    All four attacks return a valid boolean expression.

--- src/test/StartupTest.java
+++ src/test/StartupTest.java
@@ -12,15 +12,15 @@ public class StartupTest {
     
     StartupDirector startupDirector;
     StartupBuilder startupBuilder;
-    String startupTestName;
-    String startupTestName2;
-    String startupTestName3;
-    String startupTestName4;
+    String testNameMP;
+    String testNameHW;
+    String testNameSE;
+    String testNameBE;
     
-    Startup testSU;
-    Startup test2SU;
-    Startup test3SU;
-    Startup test4SU;
+    Startup testMP;
+    Startup testHW;
+    Startup testSE;
+    Startup testBE;
     
     TechGiantDirector techDirector;
     TechGiantBuilder techBuilder;
@@ -31,25 +31,25 @@ public class StartupTest {
     public void setup() throws Exception{
         startupDirector = new StartupDirector();
         startupBuilder = new StartupMP();
-        startupTestName = "Test Startup";
-        startupTestName2 = "The Other Guy";
-        startupTestName3 = "Backdoor Bros";
-        startupTestName4 = "Better Service";
+        testNameMP = "Test Startup";
+        testNameHW = "The Other Guy";
+        testNameSE = "Backdoor Bros";
+        testNameBE = "Better Service";
         
-        startupDirector.Construct(startupBuilder, startupTestName, TechType.MARKETPLACE);
-        testSU = startupBuilder.getStartup();
+        startupDirector.Construct(startupBuilder, testNameMP, TechType.MARKETPLACE);
+        testMP = startupBuilder.getStartup();
         
         startupBuilder = new StartupHW();
-        startupDirector.Construct(startupBuilder, startupTestName2, TechType.HARDWARE);
-        test2SU = startupBuilder.getStartup();
+        startupDirector.Construct(startupBuilder, testNameHW, TechType.HARDWARE);
+        testHW = startupBuilder.getStartup();
         
         startupBuilder = new StartupSE();
-        startupDirector.Construct(startupBuilder, startupTestName3, TechType.SERVICE);
-        test3SU = startupBuilder.getStartup();
+        startupDirector.Construct(startupBuilder, testNameSE, TechType.SERVICE);
+        testSE = startupBuilder.getStartup();
         
         startupBuilder = new StartupBE();
-        startupDirector.Construct(startupBuilder, startupTestName4, TechType.BUSINESSEXT);
-        test4SU = startupBuilder.getStartup();
+        startupDirector.Construct(startupBuilder, testNameBE, TechType.BUSINESSEXT);
+        testBE = startupBuilder.getStartup();
         
         techDirector = new TechGiantDirector();
         techBuilder = new TechGiantNA();
@@ -61,13 +61,13 @@ public class StartupTest {
     public void tearDown() throws Exception{
         startupDirector = null;
         startupBuilder = null;
-        startupTestName = null;
-        startupTestName2 = null;
-        startupTestName3 = null;
-        testSU = null;
-        test2SU = null;
-        test3SU = null;
-        test4SU = null;
+        testNameMP = null;
+        testNameHW = null;
+        testNameSE = null;
+        testMP = null;
+        testHW = null;
+        testSE = null;
+        testBE = null;
         
         techDirector = null;
         techBuilder = null;
@@ -78,36 +78,36 @@ public class StartupTest {
     @Test
     public void startupNameTest() {
         //Startup testSU = startupBuilder.getStartup();
-        System.out.println(testSU.getName() + " vs " + startupTestName);
-        assertEquals(testSU.getName(), startupTestName);
+        System.out.println(testMP.getName() + " vs " + testNameMP);
+        assertEquals(testMP.getName(), testNameMP);
     }
     
     @Test
     public void startupStartingLevelTest() {
         int lvl = 1;
         //Startup testSU = startupBuilder.getStartup();
-        assertEquals(testSU.getLevel(), lvl);
+        assertEquals(testMP.getLevel(), lvl);
     }
     
     @Test
     public void startupStartingApprovalTest() {
         double approve = Consts.SU_LOW;
         //Startup testSU = startupBuilder.getStartup();
-        assertEquals(testSU.getPublicApproval(), approve, 0.001);
+        assertEquals(testMP.getPublicApproval(), approve, 0.001);
     }
     
     @Test
     public void startupStartingNetIncomeTest() {
         double netIncome = Consts.SU_HIGH;
         //Startup testSU = startupBuilder.getStartup();
-        assertEquals(testSU.getNetIncome(), netIncome, 0.001);
+        assertEquals(testMP.getNetIncome(), netIncome, 0.001);
     }
     
     @Test
     public void startupStartRevenueTest() {
         double rev = Consts.SU_MID;
         //Startup testSU = startupBuilder.getStartup();
-        assertEquals(testSU.getRevenue(), rev, 0.001);
+        assertEquals(testMP.getRevenue(), rev, 0.001);
     }
     
     /**
@@ -116,7 +116,7 @@ public class StartupTest {
     @Test
     public void startupStartIndependent() {
        // Startup testSU = startupBuilder.getStartup();
-        assertNull(testSU.getOverLord());
+        assertNull(testMP.getOverLord());
     }
     
     /**
@@ -126,8 +126,8 @@ public class StartupTest {
     public void startupAddOverLord() {
         //Startup testSU = startupBuilder.getStartup();
         TechGiant testTG = techBuilder.getTechGiant();
-        testSU.setOverLord(testTG);
-        assertEquals(testTG, testSU.getOverLord());
+        testMP.setOverLord(testTG);
+        assertEquals(testTG, testMP.getOverLord());
     }
     
     /**
@@ -137,9 +137,9 @@ public class StartupTest {
     public void addAndRemoveTG() {
         //Startup testSU = startupBuilder.getStartup();
         TechGiant testTG = techBuilder.getTechGiant();
-        testSU.setOverLord(testTG);
-        testSU.makeIndependent();
-        assertNull(testSU.getOverLord());
+        testMP.setOverLord(testTG);
+        testMP.makeIndependent();
+        assertNull(testMP.getOverLord());
     }
     
     /**
@@ -147,12 +147,51 @@ public class StartupTest {
      * */
     @Test
     public void hackServerTest() {
-        boolean outcome1 = testSU.hackServer(test2SU);
+        boolean outcome1 = testMP.hackServer(testHW);
         assertNotNull(outcome1);
-        boolean outcome2 = testSU.hackServer(test3SU);
+        boolean outcome2 = testMP.hackServer(testSE);
         assertNotNull(outcome2);
-        boolean outcome3 = testSU.hackServer(test4SU);
+        boolean outcome3 = testMP.hackServer(testBE);
         assertNotNull(outcome3);
+    }
+    
+    /**
+     * Test an undercut prices attack from one startup to multiple others.
+     * */
+    @Test
+    public void undercutPricesTest() {
+        boolean outcome1 = testHW.undercutPrices(testMP);
+        assertNotNull(outcome1);
+        boolean outcome2 = testHW.undercutPrices(testBE);
+        assertNotNull(outcome2);
+        boolean outcome3 = testHW.undercutPrices(testSE);
+        assertNotNull(outcome3);
+    }
+    
+    /**
+     * Test a misinformationBlast attack from one startup to multiple others.
+     * */
+    @Test
+    public void misninformationBlastTest() {
+        boolean outcome1 = testBE.misinformationBlast(testHW);
+        boolean outcome2 = testBE.misinformationBlast(testMP);
+        boolean outcome3 = testBE.misinformationBlast(testSE);
+        assertNotNull(outcome1);
+        assertNotNull(outcome2);
+        assertNotNull(outcome3);
+    }
+    
+    /**
+     * Test a talentDrainTest() from one startup against multiple others.
+     * */
+    @Test
+    public void talentDrainTest() {
+       boolean outcome1 = testSE.talentDrain(testBE);
+       boolean outcome2 = testSE.talentDrain(testHW);
+       boolean outcome3 = testSE.talentDrain(testMP);
+       assertNotNull(outcome1);
+       assertNotNull(outcome2);
+       assertNotNull(outcome3);
     }
     
     /**
@@ -160,10 +199,10 @@ public class StartupTest {
      * */
     @Test
     public void modBE() {
-        assertEquals(test4SU.getNetIncomeMod(), Consts.MOD_ZERO);
-        assertEquals(test4SU.getRevenueMod(), Consts.MOD_HIGH);
-        assertEquals(test4SU.getPublicApprovalMod(), Consts.MOD_MID);
-        assertEquals(test4SU.getMarketShareMod(), Consts.MOD_LOW);
+        assertEquals(testBE.getNetIncomeMod(), Consts.MOD_ZERO);
+        assertEquals(testBE.getRevenueMod(), Consts.MOD_HIGH);
+        assertEquals(testBE.getPublicApprovalMod(), Consts.MOD_MID);
+        assertEquals(testBE.getMarketShareMod(), Consts.MOD_LOW);
     }
     
     /**
@@ -172,25 +211,25 @@ public class StartupTest {
     
     @Test 
     public void changeMods() {
-        test4SU.adjNetIncomeMod(1);
-        assertEquals(test4SU.getNetIncomeMod(), Consts.MOD_ZERO + 1);
-        test4SU.adjNetIncomeMod(-1);
-        assertEquals(test4SU.getNetIncomeMod(), Consts.MOD_ZERO);
+        testBE.adjNetIncomeMod(1);
+        assertEquals(testBE.getNetIncomeMod(), Consts.MOD_ZERO + 1);
+        testBE.adjNetIncomeMod(-1);
+        assertEquals(testBE.getNetIncomeMod(), Consts.MOD_ZERO);
         
-        test4SU.adjRevenueMod(1);
-        assertEquals(test4SU.getRevenueMod(), Consts.MOD_HIGH + 1);
-        test4SU.adjRevenueMod(-2);
-        assertEquals(test4SU.getRevenueMod(), Consts.MOD_MID);
+        testBE.adjRevenueMod(1);
+        assertEquals(testBE.getRevenueMod(), Consts.MOD_HIGH + 1);
+        testBE.adjRevenueMod(-2);
+        assertEquals(testBE.getRevenueMod(), Consts.MOD_MID);
         
-        test4SU.adjPublicApprovalMod(1);
-        assertEquals(test4SU.getPublicApprovalMod(), Consts.MOD_MID + 1);
-        test4SU.adjPublicApprovalMod(-2);
-        assertEquals(test4SU.getPublicApprovalMod(), Consts.MOD_LOW);
+        testBE.adjPublicApprovalMod(1);
+        assertEquals(testBE.getPublicApprovalMod(), Consts.MOD_MID + 1);
+        testBE.adjPublicApprovalMod(-2);
+        assertEquals(testBE.getPublicApprovalMod(), Consts.MOD_LOW);
         
-        test4SU.adjMarketShareMod(1);
-        assertEquals(test4SU.getMarketShareMod(), Consts.MOD_LOW + 1);
-        test4SU.adjMarketShareMod(-2);
-        assertEquals(test4SU.getMarketShareMod(), Consts.MOD_ZERO);
+        testBE.adjMarketShareMod(1);
+        assertEquals(testBE.getMarketShareMod(), Consts.MOD_LOW + 1);
+        testBE.adjMarketShareMod(-2);
+        assertEquals(testBE.getMarketShareMod(), Consts.MOD_ZERO);
     }
     
     /**
@@ -198,10 +237,10 @@ public class StartupTest {
      * */
     @Test
     public void modHW() {
-        assertEquals(test2SU.getNetIncomeMod(), Consts.MOD_HIGH);
-        assertEquals(test2SU.getRevenueMod(), Consts.MOD_MID);
-        assertEquals(test2SU.getPublicApprovalMod(), Consts.MOD_LOW);
-        assertEquals(test2SU.getMarketShareMod(), Consts.MOD_ZERO);
+        assertEquals(testHW.getNetIncomeMod(), Consts.MOD_HIGH);
+        assertEquals(testHW.getRevenueMod(), Consts.MOD_MID);
+        assertEquals(testHW.getPublicApprovalMod(), Consts.MOD_LOW);
+        assertEquals(testHW.getMarketShareMod(), Consts.MOD_ZERO);
     }
     
     /**
@@ -209,10 +248,10 @@ public class StartupTest {
      * */
     @Test
     public void modSE() {
-        assertEquals(test3SU.getNetIncomeMod(), Consts.MOD_LOW);
-        assertEquals(test3SU.getRevenueMod(), Consts.MOD_ZERO);
-        assertEquals(test3SU.getPublicApprovalMod(), Consts.MOD_HIGH);
-        assertEquals(test3SU.getMarketShareMod(), Consts.MOD_MID);
+        assertEquals(testSE.getNetIncomeMod(), Consts.MOD_LOW);
+        assertEquals(testSE.getRevenueMod(), Consts.MOD_ZERO);
+        assertEquals(testSE.getPublicApprovalMod(), Consts.MOD_HIGH);
+        assertEquals(testSE.getMarketShareMod(), Consts.MOD_MID);
     }
     
     /**
@@ -220,10 +259,10 @@ public class StartupTest {
      * */
     @Test
     public void modMP() {
-        assertEquals(testSU.getNetIncomeMod(), Consts.MOD_MID);
-        assertEquals(testSU.getRevenueMod(), Consts.MOD_LOW);
-        assertEquals(testSU.getPublicApprovalMod(), Consts.MOD_ZERO);
-        assertEquals(testSU.getMarketShareMod(), Consts.MOD_HIGH);
+        assertEquals(testMP.getNetIncomeMod(), Consts.MOD_MID);
+        assertEquals(testMP.getRevenueMod(), Consts.MOD_LOW);
+        assertEquals(testMP.getPublicApprovalMod(), Consts.MOD_ZERO);
+        assertEquals(testMP.getMarketShareMod(), Consts.MOD_HIGH);
     }
 
 }
